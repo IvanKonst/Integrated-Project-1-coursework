a7a55976f5bca4844c2581ac9f45890cbb953684
FelixMortis <47950290+FelixMortis@users.noreply.github.com>
2019-04-09 00:04:00 +0100

    spikes

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spikes : MonoBehaviour
+{
+    public int Damage { get; set; }
+
+    void Start()
+    {
+        Damage = 100;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            col.transform.GetComponent<IEnemy>().TakeDamage(Damage);
+        }
+    }
+
+}
